8edeb3bdc960d37d74677873365d8b9564dca41b
Matthew Soulanille <matthew@soulanille.net>
2018-12-04 15:46:22 -0500

    Youtube working with Justin's UI

--- App.js
+++ App.js
@@ -6,43 +6,65 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import VitaminList from './src/pages/VitaminList.js';
-import VitaminInfo from './src/pages/VitaminInfo.js';
-import Homescreen from './src/pages/Homescreen.js';
-import ScreeningList from "./src/pages/ScreeningList.js";
-import LoginScreen from "./src/pages/LoginScreen.js";
-import {createStackNavigator} from 'react-navigation';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import VitaminList from './src/screens/VitaminList.js';
+import VitaminInfo from './src/screens/VitaminInfo.js';
+import HomeScreen from './src/screens/HomeScreen.js';
+import ScreeningList from "./src/screens/ScreeningList.js";
+import LoginScreen from "./src/screens/LoginScreen.js";
+import { createStackNavigator } from 'react-navigation';
+import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar } from 'react-native';
+import MainTabNavigator from './src/navigation/MainTabNavigator';
+
+// export default createStackNavigator({
+
+//     Home: { screen: HomeScreen },
+//     Screening: { screen: ScreeningList },
+//     AllVitamins: { screen: VitaminList },
+//     VitaminInfo: { screen: VitaminInfo },
+//     Login: { screen: LoginScreen }
+// });
 
 
-
-const App = createStackNavigator({
-    
-    Home: {screen: Homescreen},
-    Screening: {screen: ScreeningList},
-    AllVitamins: {screen: VitaminList},
-    VitaminInfo: {screen: VitaminInfo},
-    Login: {screen: LoginScreen}
+var TestStack = createStackNavigator({
+    Login: LoginScreen
 });
+// export default TestStack;
 
-export default App;
+export default AppNavigator;
+
+// export default class App extends React.Component {
+//     render() {
+//         return (
+//             <View style={styles.container}>
+//               {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+//               <TestStack/>
+//               <AppNavigator />
+//             </View>
+//         );
+//     }
+// }
+
+
+//export default App;
 
 const styles = StyleSheet.create({
     container: {
-	flex: 1,
-	justifyContent: 'center',
-	alignItems: 'center',
-	backgroundColor: '#F5FCFF',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
     },
     welcome: {
-	fontSize: 20,
-	textAlign: 'center',
-	margin: 10,
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
     },
     instructions: {
-	textAlign: 'center',
-	color: '#333333',
-	marginBottom: 5,
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
     },
 });
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+            <Image
+              source={ require('../../assets/images/logo.png')}
+              style={styles.welcomeImage}
+            />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    resizeMode: 'stretch'
+  },
+  
+  welcomeImage: {
+    width: 300,
+    height: 300,
+    alignSelf: 'center',
+  },
+});
